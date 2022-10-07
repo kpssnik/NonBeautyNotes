f40060821ee80f6f39e32c03dd22c30f15dd891f
kpssnik <kpssdeveloper@gmail.com>
2022-10-07 17:37:46 +0300

    Navbar ADD button, which creating new card. Layour and JS scripts for creating it. Vertical card overflow-y added

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const cardsBlock = document.querySelector('.flexbox');
+const addCardButton = document.querySelector('.add-card-btn');
+
+// new card after clicking ADD
+function getNewCard(title, content, styleClass) {
+
+    // creating new card
+    const newCard = document.createElement('div');
+
+    newCard.classList.add('card');
+    if (styleClass !== undefined)
+        newCard.classList.add(styleClass);
+
+    newCard.innerHTML = `
+        <div class="card-header">
+            <h3>${title}</h3>
+            <div>
+                <button class="btn edit-card-btn"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button class="btn edit-card-btn"><i class="fa-sharp fa-solid fa-trash"></i></button>
+            </div>
+        </div>
+        <div class="card-main">
+            <p class="card-main-content">
+                ${content}
+            </p>
+        </div>
+        <div class="card-footer">
+            <button class="style-btn red-card-style">RED</button>
+            <button class="style-btn yellow-card-style">YLW</button>
+            <button class="style-btn green-card-style">GRN</button>
+            <button class="style-btn default-card-style">DEF</button>
+        </div>
+    `;
+
+    // TODO edit delete  event listeners //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    return newCard;
+}
+
+addCardButton.addEventListener('click', event => {
+
+    cardsBlock.appendChild(getNewCard('Title', 'Content', 'default-card-style'));
+});

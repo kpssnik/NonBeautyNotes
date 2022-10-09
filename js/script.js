d176ec0477230ad2cc0e2817b8638a385cf49ba5
kpssnik <kpssdeveloper@gmail.com>
2022-10-09 15:12:53 +0300

    implemented saving cards to local storage and loading them after page refresh (needs refactoring

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 // card container
 const cardsBlock = document.querySelector('.flexbox');
-
 // left top button adding new card
 const addCardButton = document.querySelector('.add-card-btn');
 
-// new card after clicking ADD
-function getNewCard(title, content, styleClass) {
+// return card element with given title, content and style
+function getNewCard(title, content, cardStyle) {
 
     // creating new card
     const newCard = document.createElement('div');
 
     newCard.classList.add('card');
-    if (styleClass !== undefined)
-        newCard.classList.add(styleClass);
+
+    // cardStyle is attribute, it is needed to save it to local storage
+    // converting attribute to classname and adding
+    const classStyle = cardStyle + '-card-style';
+    newCard.classList.add(classStyle);
+
+    newCard.setAttribute('card-style', cardStyle);
 
     newCard.innerHTML = `
         <div class="card-header">
@@ -41,12 +45,65 @@ function getNewCard(title, content, styleClass) {
 
     deleteBtn.addEventListener('click', () => {
         newCard.remove();
+        updateLocalStorage();
     });
 
     return newCard;
 }
 
-addCardButton.addEventListener('click', event => {
+// transform given cardElement to object
+function cardToObject(cardElement) {
 
-    cardsBlock.appendChild(getNewCard('Title', 'Content', 'default-card-style'));
+    const titleElement = cardElement.querySelector('.card-header h3');
+    const contentElement = cardElement.querySelector('.card-main-content');
+    const cardStyle = cardElement.getAttribute('card-style');
+
+    return {
+        title: titleElement.innerHTML,
+        content: contentElement.innerHTML,
+        style: cardStyle
+    };
+}
+
+// transform all html cards to object array
+function getCardObjectArray() {
+
+    let result = [];
+
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+
+        const cardObj = cardToObject(card);
+        result.push(cardObj);
+    });
+
+    return result;
+}
+
+// rewrite local storage (calling after adding/deleting/editing card)
+function updateLocalStorage() {
+    localStorage.cardsData = JSON.stringify(getCardObjectArray());
+}
+
+// render cards from local storage (after loading page)
+function renderCardsFromStorage() {
+
+    let cards = JSON.parse(localStorage.cardsData);
+
+    cards.forEach(card => {
+        cardsBlock.append(getNewCard(card.title, card.content, card.style));
+    });
+}
+
+
+addCardButton.addEventListener('click', () => {
+    cardsBlock.appendChild(getNewCard('Title', 'Content', 'default'));
+
+    updateLocalStorage();
 });
+
+
+
+
+
